d0d9d88a860adb783e184a9760bf9cf67973cde1
222112127 <222112127@stis.ac.id>
2023-12-09 13:53:16 +0700

    pertemuan 14 part 1 SQLite

---
+++ app/src/main/java/com/manda/sqlite/ViewMahasiswa.java
@@ -0,0 +1,35 @@
+package com.manda.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class ViewMahasiswa extends AppCompatActivity {
+
+    private ArrayList<MahasiswaModal>
+            mahasiswaModalArrayList;
+    private DBHandler dbHandler;
+    private MahasiswaRVAdapter mahasiswaRVAdapter;
+    private RecyclerView mahasiswaRV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_mahasiswa);
+
+        mahasiswaModalArrayList = new ArrayList<>();
+        dbHandler = new DBHandler(ViewMahasiswa.this);
+
+        mahasiswaModalArrayList = dbHandler.readMahasiswa();
+
+        mahasiswaRVAdapter = new MahasiswaRVAdapter(mahasiswaModalArrayList, ViewMahasiswa.this);
+        mahasiswaRV = findViewById(R.id.idRVMahasiswa);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ViewMahasiswa.this, RecyclerView.VERTICAL, false);
+        mahasiswaRV.setLayoutManager(linearLayoutManager);
+        mahasiswaRV.setAdapter(mahasiswaRVAdapter);
+    }
+}
